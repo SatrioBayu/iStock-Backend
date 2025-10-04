9f8f197a9995d80b8435866352e0a62d97ecffa7
unknown <sbayu875@gmail.com>
2025-10-04 20:42:39 +0800

    feat: setup db for deployment

---
+++ config/config.js
@@ -0,0 +1,28 @@
+require("dotenv").config(); // Load environment variables from .env file
+
+module.exports = {
+  development: {
+    username: "postgres",
+    password: "admin",
+    database: "istock",
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+  production: {
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
+};
